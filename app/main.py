7731021ec60ef10d204623e527e3b1a670b00479
Michael Steinmetz <torrid.luna@gmail.com>
2021-07-06 13:39:34 +0200

    read environment

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from subprocess import Popen
 from psutil import cpu_percent,getloadavg
+from os import environ
+
 app = Flask(__name__)
 
 STRESSTIME=60
 pod="main"
-# 
+
 @app.route("/")
 def hello():
     return "Hello from Python!"
@@ -20,6 +22,13 @@ def cpu():
     # out=cpu_percent(interval=0.2)
     out=getloadavg()[0]
     return "CPU load: %s" %(out)
+
+@app.route("/insight")
+def insight():
+env=environ()
+	return env
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
