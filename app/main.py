28eefe2e192bd50356bb67b39605e22276c5ec4f
Michael Steinmetz <torrid.luna@gmail.com>
2021-07-06 06:16:34 +0000

    load and stress functions

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
 from flask import Flask
+from subprocess import Popen
+from psutil import cpu_percent,getloadavg
 app = Flask(__name__)
 
 STRESSTIME=60
-
+pod="main"
+# 
 @app.route("/")
 def hello():
     return "Hello from Python!"
 
 @app.route("/stress")
 def stress():
-    os.system("/usr/bin/stress --cpu 2 --timeout 2")
-    out=subprocess.Popen("/usr/bin/stress --cpu 1 --timeout %s" \
-        %(STRESSTIME).split())
-    return "60 Seconds CPU stress.\n\rResult: " + out
+    out=Popen(["/usr/bin/stress", "--cpu", "1", "--timeout", "%s"%STRESSTIME])
+    return "60 Seconds CPU stress."
+
+@app.route("/cpu")
+def cpu():
+    # out=cpu_percent(interval=0.2)
+    out=getloadavg()[0]
+    return "CPU load: %s" %(out)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
